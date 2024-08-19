e2b05d0afc3bb5367cb1ce574abfdf634c366ddd
ISMAEL44412 <93667525+ISMAEL44412@users.noreply.github.com>
2024-08-19 17:08:10 -0300

    Update urls.py

--- django-mysql-crud-main/mi_proyecto/cruddemo/urls.py
+++ django-mysql-crud-main/mi_proyecto/cruddemo/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('simpleweb/',include('simpleweb.urls')),
+    path('',include('simpleweb.urls')),
     path('admin/', admin.site.urls),
 ]
